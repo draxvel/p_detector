616ce4ebad377b73d0c10a503f6a27fd91888dae
nazar.tkachuk <nazar.tkachuk@vakoms.com.ua>
2019-07-15 14:12:18 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/tkachuk/p_detector/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.tkachuk.p_detector
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
